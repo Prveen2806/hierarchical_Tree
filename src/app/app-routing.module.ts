f958095cba0a523745c10a04fcc0f9458cc0595a
Praveen NS <praveenns@gramosoft.in>
2025-11-01 13:11:14 +0530

    second commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TreeComponent } from './components/tree/tree.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+   { path: '', redirectTo: 'tree', pathMatch: 'full' },
+  { path: 'tree', component: TreeComponent },
+  { path: '**', redirectTo: 'tree' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
